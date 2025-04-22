65886e2fe0a56a5b71b8dc21f5def0ecec624b81
jie.zhang22 <jie.zhang22@envision-energy.com>
2025-04-22 18:38:57 +0800

    读取文件，添加主机至对应主机群组

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-# ZABBIX_URL = "http://mm.envisioncn.com/zabbix"
-# ZABBIX_USER = "Admin"
-# ZABBIX_PASSWORD = "Wh2i@_%3uTo4njaP"
+ZABBIX_URL = "http://mm.envisioncn.com/zabbix"
+ZABBIX_USER = "Admin"
+ZABBIX_PASSWORD = "Wh2i@_%3uTo4njaP"
 
-ZABBIX_URL = "https://sysmonitor.envisioncn.com/"
-ZABBIX_USER = "r_jie.zhang22"
-ZABBIX_PASSWORD = "Ws&egY@7X_3F"
+# ZABBIX_URL = "https://sysmonitor.envisioncn.com/"
+# ZABBIX_USER = "r_jie.zhang22"
+# ZABBIX_PASSWORD = "Ws&egY@7X_3F"
 
 #  [AD]
 #  ldap_server = 10.86.8.45
--- hostgroup.py
+++ hostgroup.py
@@ -6,25 +6,43 @@ class Hostgroup:
         self.zabbix_api = ZabbixAPI()
 
     def get_hostgroup_info(self, group_name):
-        """获取主机组信息"""
+        """获取主机组信息（返回支持中文的 JSON 字符串）"""
         params = {
             "filter": {"name": group_name},
             "output": ["groupid", "name"]
         }
         response = self.zabbix_api.call_api("hostgroup.get", params)
+        # … 原有逻辑 …
 
-        if response.get("result"):  # 确保返回了有效的结果
-            group_info = {
-                "group_id": response["result"][0]["groupid"],
-                "name": response["result"][0]["name"]
-            }
-            return json.dumps(group_info, indent=4)  # 返回格式化的JSON字符串
-        else:
-            return json.dumps({})  # 返回空的JSON对象
+    def massupdate(self, params):
+        """
+        将指定 IP 的主机添加到指定的 Zabbix 主机组中。
+        params: {
+            "ip": "10.86.x.x",
+            "group": "组名"
+        }
+        """
+        ip = params.get("ip")
+        group_name = params.get("group")
 
-# # 调用示例
-# if __name__ == "__main__":
-#     hostgroup = Hostgroup()
-#     group_name = "硬件_Dell"  # 需要查询的主机组名称
-#     group_info_json = hostgroup.get_hostgroup_info(group_name)
-#     print(group_info_json)
+        # 1. 获取主机组 ID
+        group_resp = self.zabbix_api.call_api("hostgroup.get", {
+            "filter": {"name": [group_name]},
+            "output": ["groupid"]
+        })
+        groupid = group_resp["result"][0]["groupid"]  # :contentReference[oaicite:3]{index=3}
+
+        # 2. 获取主机 ID（通过接口 IP）
+        iface_resp = self.zabbix_api.call_api("hostinterface.get", {
+            "output": ["interfaceid"],
+            "filter": {"ip": ip},
+            "selectHosts": ["hostid"]
+        })
+        hostid = iface_resp["result"][0]["hosts"][0]["hostid"]  # :contentReference[oaicite:4]{index=4}
+
+        # 3. 调用 host.massadd，将主机添加到组中
+        add_resp = self.zabbix_api.call_api("host.massadd", {
+            "hosts": [{"hostid": hostid}],
+            "groups": [{"groupid": groupid}]
+        })
+        return add_resp  # :contentReference[oaicite:5]{index=5}
